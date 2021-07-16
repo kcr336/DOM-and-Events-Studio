18b3e8b563e24c118dbcb779a081824861f419c9
kcr336 <46976285+kcr336@users.noreply.github.com>
2021-07-15 19:46:57 -0500

    step 2

--- scripts.js
+++ scripts.js
@@ -1,2 +1,26 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+window.addEventListener("load", function(){
+    let status = this.document.getElementById("flightStatus");
+    let bg = this.document.getElementById("shuttleBackground");
+    let shuttleHeight = this.document.getElementById("spaceShuttleHeight");
+
+    let takeoff = this.document.getElementById('takeoff');
+    takeoff.addEventListener("click", function(){
+        if (window.confirm("Confirm that the shuttle is ready for takeoff.")){
+            status.innerHTML = "Shuttle in flight";
+            bg.style.backgroundColor='blue';
+            height.innerHTML = '10000'
+        }
+    })
+
+    let landing = this.document.getElementById("landing");
+    landing.addEventListener("click", function(){
+        if (window.confirm("The shuttle is landing. Landing gear engaged.")){
+            status.innerHTML = "The shuttle has landed.";
+            bg.style.backgroundColor='green';
+            height.innerHTML = '0'
+        }
+    })
+})
